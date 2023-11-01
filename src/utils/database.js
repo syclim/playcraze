becdf08c8d7e77c0bf3c4a031ddcacb76d3fef0e
Jesse Uszkay <131556370+syclim@users.noreply.github.com>
2023-11-01 15:53:27 -0400

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export function obtainGameList(gameFilters, gameListFunction) {
+  const apiURL = import.meta.env.VITE_API_URL;
   const url_filters =
     "/" +
     (gameFilters.search ? gameFilters.search + "&" : "&") +
@@ -8,7 +9,7 @@ export function obtainGameList(gameFilters, gameListFunction) {
     (gameFilters.platform ? gameFilters.platform : "");
 
   axios
-    .get(`http://localhost:8080/games/${url_filters}`)
+    .get(`${apiURL}${url_filters}`)
     .then((response) => {
       gameListFunction(response.data.results.slice(0, 10));
     })
